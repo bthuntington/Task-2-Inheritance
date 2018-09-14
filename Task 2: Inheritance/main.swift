f83b2a42a27ff9be11e1c8f77d47da46dd073464
Brooke <bhuntington@zagmail.gonzaga.edu>
2018-09-14 14:12:07 -0700

    Initial Commit

---
+++ Task 2: Inheritance/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task 2: Inheritance
+//
+//  Created by Brooke Huntington on 9/14/18.
+//  Copyright © 2018 Brooke Huntington. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
